183bc13c1c1457f5dc3eaf9e8559bf2b504f4265
Jeevan-Rai <78399976+Jeevan-Rai@users.noreply.github.com>
2022-01-30 15:23:11 +0530

    checkout edited

--- main.py
+++ main.py
@@ -348,18 +348,19 @@ def manage():
     
 @app.route('/cart')
 def cart():
-    try:
+    # try:
         custid = db.session.query(Customer.CUST_ID).filter_by(USERNAME=params['crnt_usr']).all()
         cid = db.session.query(Cart.CART_ID).filter_by(CUST_ID=custid[0][0]).all()
         pid = db.session.query(Cart_products.P_ID).filter_by(CART_ID=cid[0][0])
         prdcts = db.session.query(Products).filter_by(P_ID=pid).all()
         return render_template('c_cart.html',prdcts=prdcts)
-    except:
+    # except:
         return redirect('/')
 
 @app.route('/payment/<string:pid>')
 def payment(pid):
     prdcts = db.session.query(Products).filter_by(P_ID=pid)
-    
+    return render_template('payment.html')
+
 
 app.run(debug=True)
